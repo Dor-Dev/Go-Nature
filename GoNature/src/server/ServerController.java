349d62a1c0c01d265e0a9ec7a0aa9d9f745281c5
amit <amit@LAPTOP-5GEQ45DR>
2020-12-18 21:22:46 +0200

    for amit

--- GoNature/src/server/ServerController.java
+++ GoNature/src/server/ServerController.java
@@ -7,8 +7,10 @@ package server;
 import java.io.*;
 
 import gui.ServerGUIController;
+import ocsf.server.AbstractServer;
+import ocsf.server.ConnectionToClient;
 import common.Message;
-import ocsf.server.*;
+
 
 /**
  * This class overrides some of the methods in the abstract superclass in order
