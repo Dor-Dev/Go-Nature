6792955b9afe23f0c429d8e51e767af5eca93456
Naor0 <Naor0@DESKTOP-RVLRFSI>
2020-12-21 01:21:48 +0200

    add all combobox option in AddOrder.
    Discount is changing by click on Group/Single-Family.
    
    bug fixed
    
    remove the pre-order view "Visitor amount" beacuse its shown on screen
    by chosing.
    
    ---Need to fix the Regular traveler login, didnt work beacuse of
    "MenuBarSelection", this class is adapted only to employee need to cover
    case of travelers. will done tommorow.

--- GoNatureClient/src/gui/AddOrderGUIController.java
+++ GoNatureClient/src/gui/AddOrderGUIController.java
@@ -4,79 +4,157 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AddOrderGUIController {
 	@FXML
-    private Label mnuRequests;
-    @FXML
-    private Label mnuAddOrder;
+	private Label mnuRequests;
+	@FXML
+	private Label mnuAddOrder;
 
-    @FXML
-    private Label mnuMyOrders;
+	@FXML
+	private Label mnuMyOrders;
 
-    @FXML
-    private Label mnuMyProfile;
+	@FXML
+	private Label mnuMyProfile;
 
-    @FXML
-    private Label mnuParkEntrance;
+	@FXML
+	private Label mnuParkEntrance;
 
-    @FXML
-    private Label mnuRegistration;
+	@FXML
+	private Label mnuRegistration;
 
-    @FXML
-    private Label mnuReportsManager;
+	@FXML
+	private Label mnuReportsManager;
 
-    @FXML
-    private Label mnuEvents;
+	@FXML
+	private Label mnuEvents;
 
-    @FXML
-    private Label mnuParkDetails;
+	@FXML
+	private Label mnuParkDetails;
 
-    @FXML
-    private Label mnuParkCapacity;
+	@FXML
+	private Label mnuParkCapacity;
 
+	@FXML
+	private Label mnuReportsDepartment;
+
+	@FXML
+	private ComboBox<String> cmbParkName;
+
+	@FXML
+	private ComboBox<String> cmbHour;
+
+	@FXML
+	private ComboBox<String> cmbNumOfVisitors;
+
+	@FXML
+	private TextField txtEmail;
+
+	@FXML
+	private Label lblDiscount;
+
+	@FXML
+	private Label lblTotalPrice;
+
+	@FXML
+	private Button btnSubmitOrder;
+
+	@FXML
+	private RadioButton rdSingleFamily;
+
+	@FXML
+	private ToggleGroup visitorType;
+
+	@FXML
+	private RadioButton rdGroup;
+
+	public void show() {
+		VBox root;
+		Stage primaryStage = new Stage();
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/gui/AddOrderGUI.fxml"));
+			root = loader.load();
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Add Order");
+			AddOrderGUIController addOrderController = loader.getController();
+
+			List<Label> menuLabels = new ArrayList<>();
+			menuLabels = createLabelList(addOrderController);
+			MenuBarSelection.setMenuOptions(menuLabels);
+			primaryStage.show();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+	}
+
+	private List<Label> createLabelList(AddOrderGUIController addOrderController) {
+		List<Label> tempMenuLabels = new ArrayList<>();
+		tempMenuLabels.add(addOrderController.mnuAddOrder);
+		tempMenuLabels.add(addOrderController.mnuMyOrders);
+		tempMenuLabels.add(addOrderController.mnuMyProfile);
+		tempMenuLabels.add(addOrderController.mnuParkEntrance);
+		tempMenuLabels.add(addOrderController.mnuRegistration);
+		tempMenuLabels.add(addOrderController.mnuParkDetails);
+		tempMenuLabels.add(addOrderController.mnuEvents);
+		tempMenuLabels.add(addOrderController.mnuReportsDepartment);
+		tempMenuLabels.add(addOrderController.mnuReportsManager);
+		tempMenuLabels.add(addOrderController.mnuParkCapacity);
+		tempMenuLabels.add(addOrderController.mnuRequests);
+		return tempMenuLabels;
+	}
+
+	@FXML
+	public void initialize() {
+		/**
+		 * Initialize combo box Parks name
+		 */
+		cmbParkName.getItems().removeAll(cmbParkName.getItems());
+		cmbParkName.getItems().addAll("Luna-Park", "Shipment-Park", "Tempo-Park");
+		// cmbParkName.getSelectionModel().select("Option B");
+
+		/**
+		 * Initialize cmb Visit Hour 10:00 - 17:00
+		 */
+		cmbHour.getItems().removeAll(cmbHour.getItems());
+		cmbHour.getItems().addAll("10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00");
+
+		/**
+		 * Initialize cmb Num of visitors 1-15
+		 */
+		cmbNumOfVisitors.getItems().removeAll(cmbNumOfVisitors.getItems());
+		cmbNumOfVisitors.getItems().addAll("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14",
+				"15");
+		
+
+		//rdGroup.setDisable(true);
+	}
+	
+	/**
+	 * update pre-order discount at window in red
+	 * @param event
+	 */
     @FXML
-    private Label mnuReportsDepartment;
-	    
-	 public void show() 
-	    {
-	    	VBox root;
-	    	Stage primaryStage = new Stage();
-			try {
-				FXMLLoader loader = new FXMLLoader();
-				loader.setLocation(getClass().getResource("/gui/AddOrderGUI.fxml"));
-				root = loader.load();
-				Scene scene = new Scene(root);
-				primaryStage.setScene(scene);
-				primaryStage.setTitle("Add Order");
-				AddOrderGUIController addOrderController = loader.getController();
-				
-				List<Label> menuLabels = new ArrayList<>();
-				menuLabels.add(addOrderController.mnuAddOrder);
-				menuLabels.add(addOrderController.mnuMyOrders);
-				menuLabels.add(addOrderController.mnuMyProfile);
-				menuLabels.add(addOrderController.mnuParkEntrance);
-				menuLabels.add(addOrderController.mnuRegistration);
-				menuLabels.add(addOrderController.mnuParkDetails);
-				menuLabels.add(addOrderController.mnuEvents);
-				menuLabels.add(addOrderController.mnuReportsDepartment);
-				menuLabels.add(addOrderController.mnuReportsManager);
-				menuLabels.add(addOrderController.mnuParkCapacity);
-				menuLabels.add(addOrderController.mnuRequests);
-				MenuBarSelection.setMenuOptions(menuLabels);
-				primaryStage.show();
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-				return;
-			}
-	    }
-	 
+    void updateGroupDiscount(ActionEvent event) {
+    	if(rdGroup.isSelected())
+    		lblDiscount.setText("20%");
+    	if(rdSingleFamily.isSelected())
+    		lblDiscount.setText("15%");
+    }
 
 }
